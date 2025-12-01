df81d93d9e4412128cb4d40ee7553b80458c564a
Damian Grzywacz <damian.grzywacz17@gmail.com>
2025-12-01 22:07:57 +0100

    Up to ex 2j

--- list7/src/Main.java
+++ list7/src/Main.java
@@ -11,7 +11,8 @@ public class Main {
         //ex2d();
         //ex2e();
         //ex2f();
-        System.out.println(countVowels("aoe035eeecdg"));
+        //System.out.println(countVowels("aoe035eeecdg"));
+        //System.out.println(isPrime(4));
     }
 
 
@@ -212,6 +213,19 @@ public class Main {
         return count;
     }
 
+    public static boolean isPrime(int number) {
+        if (number <= 1) return false;
+        if (number == 2) return true;
+        if (number % 2 == 0) return false;
 
+        for (int i = 3; i * i <= number; i += 2) {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static int findSecondLargest(int[] array) {
+
+    }
 
 }
